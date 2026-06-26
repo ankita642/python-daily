3551fc28ee92114528881e6f19fc28fd8420beda
ankita642 <jadhavankita116@gmail.com>
2026-06-26 16:20:36 +0530

    initial commit

---
+++ day2.py
@@ -0,0 +1,16 @@
+marks = int(input("enter the marks"))
+
+
+if marks>=90:
+  print("Excellent")
+
+elif marks>=75:
+   print("Good")
+
+elif marks>=40:
+   print("Fair")
+
+else:
+   print("Bad")
+
+   
