0a0e5c0a0f60370f759436e516f381af6ed9cc29
ankita642 <jadhavankita116@gmail.com>
2026-06-27 21:55:47 +0530

    initial commit

---
+++ Day3/forloop.py
@@ -0,0 +1,9 @@
+''' Given the list temperatures = [23, 45, 12, 56, 32, 19, 60],
+write a loop that evaluates each element and prints only the
+values greater than 35.'''
+
+temperatures = [23, 45, 12, 56, 32, 19, 60]
+
+for temp in temperatures:
+    if (temp>35):
+     print(temp)
