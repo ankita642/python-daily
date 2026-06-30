b026ea7ab425b41ac1f63027807c4dbca737d571
ankita642 <jadhavankita116@gmail.com>
2026-06-30 22:53:36 +0530

    initial changes

---
+++ Daily-challenges/Day6/string5.py
@@ -0,0 +1,9 @@
+#Changes text casing using methods like .upper(), .lower(), and .title()
+'''sentence = "welcome to python"
+print(sentence.title())'''
+
+'''sentence = "welcome to python"
+print(sentence.upper())'''
+
+sentence = "welcome to python"
+print(sentence.lower())
