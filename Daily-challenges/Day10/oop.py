12d5fdbb7e6a8610feba19cd221956933af05d9a
ankita642 <jadhavankita116@gmail.com>
2026-07-05 19:37:27 +0530

    initial changes

---
+++ Daily-challenges/Day10/oop.py
@@ -0,0 +1,16 @@
+'''create student class that takes name & marks of 3 subjects as
+ arguments in constructor. then create a method to print average.'''
+
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def get_avg(self):
+        sum = 0 
+        for val in self.marks:
+            sum += val
+        print("hi", self.name, "your avg score is:", sum/3)
+
+s1 = Student("ankita", [99,98,97])
+s1.get_avg()
