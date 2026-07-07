1345be29e2797aaafb19955d76bb004796727c86
ankita642 <jadhavankita116@gmail.com>
2026-07-07 14:12:08 +0530

    initial changes

---
+++ Daily-challenges/Day12/singleinhe.py
@@ -0,0 +1,22 @@
+class Car:
+    color = "black" 
+    @staticmethod
+    def start():
+        print("car started...")
+
+    @staticmethod
+    def stop():
+        print("car stoped...")
+
+class ToyotaCar(Car):
+    def__init__(self,name):
+    self.name = name
+    
+car1 = ToyotaCar("fortuner")
+car2 = ToyotaCar("prius")
+
+print(car1.start())
+print(car1.color)
+    
+        
+    
