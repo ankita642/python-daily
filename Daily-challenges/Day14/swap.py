f79767fa64090e2192695afc2eb8ce4418061819
ankita642 <jadhavankita116@gmail.com>
2026-07-10 20:41:54 +0530

    initial changes

---
+++ Daily-challenges/Day14/swap.py
@@ -0,0 +1,7 @@
+var1 = input("enter the first variable:")
+var2 = input("enter the second variable:")
+
+var1, var2 = var2, var1
+
+print("first variable after swap:",var1)
+print("second variable after swap:",var2)
