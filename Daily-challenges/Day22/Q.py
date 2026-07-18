a8f91933fc49cff3524a4f2eec873ae3167f8bb7
ankita642 <jadhavankita116@gmail.com>
2026-07-18 08:54:25 +0530

    initial changes

---
+++ Daily-challenges/Day22/Q.py
@@ -0,0 +1,23 @@
+class Employee:
+    def __init__(self, role, department, salary):
+        self.role = role
+        self.department = department
+        self.salary = salary
+
+    def showDetails(self):
+        print("role =",self.role)
+        print("depertment=",self.department)
+        print("salary=",self.salary)
+
+class Engineer(Employee):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super().__init__("Engineer","IT","75,000")       
+        
+
+
+e1 = Engineer("Ankita","20")
+e1.showDetails()
+
+
